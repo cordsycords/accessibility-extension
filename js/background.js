9509420d7536f4df58f627ff1a5046a02b890f6c
cstaffieri <cordelia.staffieri@gmail.com>
2019-09-11 16:35:34 -0400

    Initial Commit

---
+++ js/background.js
@@ -0,0 +1,8 @@
+
+//This runs on installation
+chrome.runtime.onInstalled.addListener(function()   {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+        console.log('The color is green.');
+    })
+});
+
