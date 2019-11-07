5554f87465f2edf750b7651353d470e9f7057e44
Null Null <ndna97@outlook.com>
2019-11-06 23:34:43 -0500

    didn't figure out diddly squat

--- js/dyslexia.js
+++ js/dyslexia.js
@@ -1,7 +1,6 @@
 $(function() {
     var attrs = { };
-    const maincolor = document.body.style.backgroundColor;
-    alert("COlor is: " + maincolor.toString());
+    var backgroundColorChange = "#fff000";//rgb(255,255,0);
     $("i").replaceWith(function () {
         return $("<strong />", attrs).append($(this).contents());
     });
@@ -9,7 +8,55 @@ $(function() {
     $("em").replaceWith(function () {
         return $("<strong />", attrs).append($(this).contents());
     });
-    // $(function(){
+
+   $(":visible").find('*').each(function(){ 
+        //var currentBackgroundColor = RGBToHex($(this).css("background-color"));
+        //var newBackgroundColor =   subHexColor(currentBackgroundColor, backgroundColorChange);
+        
+        //alert( newBackgroundColor);
+        $(this).css("background-color", backgroundColorChange);
+        $(this).css({"font-family": "Arial, Helvetica, sans-serif","word-spacing": "10",});
+        $(this).css("color", "#000000");
+    })
+
+    $("p").find('*').each(function(){
+        //if($(this).css("line-height", "1.5"))
+        $(this).css("line-height", "1.5");
+        //}
+    })
+});
+/* function addHexColor(c1, c2) {
+    var hexStr = (parseInt(c1, 16) + parseInt(c2, 16)).toString(16);
+    while (hexStr.length < 6) { hexStr = '0' + hexStr; } // Zero pad.
+   // alert(hexStr);
+    return hexStr;
+  }
+function subHexColor(c1, c2) {
+    var hexStr = (parseInt(c1, 16) - parseInt(c2, 16)).toString(16);
+    while (hexStr.length < 6) { hexStr = '0' + hexStr; } // Zero pad.
+    return hexStr;
+}
+ */
+/* function RGBToHex(rgb) {
+// Choose correct separator
+let sep = rgb.indexOf(",") > -1 ? "," : " ";
+// Turn "rgb(r,g,b)" into [r,g,b]
+rgb = rgb.substr(4).split(")")[0].split(sep);
+
+let r = (+rgb[0]).toString(16),
+    g = (+rgb[1]).toString(16),
+    b = (+rgb[2]).toString(16);
+
+if (r.length == 1)
+    r = "0" + r;
+if (g.length == 1)
+    g = "0" + g;
+if (b.length == 1)
+    b = "0" + b;
+
+return "#" + r + g + b;
+} */
+      // $(function(){
     //$("p").css("background-color","red");
     //$(":header").css("background-color","red");
     //$(":button").css("background-color","red");
@@ -21,27 +68,8 @@ $(function() {
     //$(":text").css("background-color","red");
         ///const style = getComputedStyle(element)
    //style.backgroundColor //the RGB value
-    $(":visible").find('*').each(function(){
-        //if($(this).is(":empty")){}else 
-        if($(this).css('background-image') != 'none'){
-          //  alert('There is a background image');
-        }
-        else{ if($(this).css('background-color') == maincolor){
-            $(this).css("background", "#ffff00")
-            $(this).css({"font-family": "Arial, Helvetica, sans-serif","word-spacing": "10",});
-            $(this).css("color", "#000000");
-        }
-       
-    //    }
-    })
-
-    $("p").find('*').each(function(){
-        //if($(this).css("line-height", "1.5"))
-        $(this).css("line-height", "1.5");
-    //    }
-    })
-
-   // $(document).ready(function(){    
+   //document.body.style.backgroundColor = "#ffff00";
+      // $(document).ready(function(){    
     //});
     //.css("background-color","red");
     //$(":root").css("background-color","red");
@@ -59,7 +87,7 @@ $(function() {
     //    }
     //    $
     //});
-    document.querySelectorAll("div")
+   // document.querySelectorAll("div")
     //var style = document.createElement('style');
     //document.head.appendChild(style);
     //style.sheet.insertRule('#target {color: darkseagreen}');
@@ -95,4 +123,11 @@ $(function() {
 
     //    }
 */
-});
+/* 
+    var currentNode,
+    ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_ELEMENT);
+ */
+/*     while(currentNode = ni.nextNode()) {
+        currentNode.style.
+        console.log(currentNode.nodeName);
+    } */
