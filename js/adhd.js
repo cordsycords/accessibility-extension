a24c7c09a89a7c9a5f460651380459e7e72882fc
Windchild292 <>
2019-11-10 13:20:32 -0500

    Blinking and Moving Text: Marquee replacement

--- js/adhd.js
+++ js/adhd.js
@@ -1,1 +1,8 @@
 //ADHD Accessibility Extension JavaScript Code Module
+$(function() {
+    var attrs = { };
+
+    $("marquee").replaceWith(function(){
+        return $("<div />", attrs).append($(this).contents());
+    });
+});
