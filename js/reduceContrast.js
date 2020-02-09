87942d3b40ba17808613e79bddd9a167df3ab57a
cstaffieri <cordelia.staffieri@gmail.com>
2020-02-09 10:43:05 -0500

    Update

---
+++ js/reduceContrast.js
@@ -0,0 +1,37 @@
+$(function () {
+
+    var attrs = { };
+    var hexcolour = '';
+    var backgroundColorChange = "#FFFFF0";
+    var rgbRegex = /rgb\(25[0-6],\s25[0-6],\s25[0-6]\)/g;
+    var rgbaRegex = /rgba\(25[0-6],\s25[0-6],\s25[0-6],\s(\d*\.\d+),?\)/g;
+    var doubleSpaceRegex = / +/g;
+    var doubleSpace = "  ";
+
+    $(":visible").find('*').each(function () {
+        var colour = $(this).css("background-color");
+        //console.log(colour);
+
+        if (colour.match(rgbRegex) != null) {
+            $(this).css("background-color", backgroundColorChange);
+            console.log($(this).css("background-color"));
+        } else if (colour.match(rgbaRegex) != null) {
+            var [r, g, b, a] = colour.match(/[\d\.]+/g);;
+            var newColour = "rgba(255, 255, 240, ${a})";
+            $(this).css("background-color", newColour);
+        }
+
+    })
+
+
+});
+
+function hexc(colorval) {
+    var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    delete (parts[0]);
+    for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+    }
+    hexcolour = '#' + parts.join('');
+}
